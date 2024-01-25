a385bca7eb782bf0a09c401875d18847b43c3d8c
Joshua Hartwig <joshuahartwig@Joshuas-MBP.home.lan>
2024-01-24 20:07:37 -0600

    fixed button width in todolist

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import { useState } from 'react'
 import Todo from './Todo'
 import NewTodoDialog from './NewTodoDialog'
+import { Button } from './ui/button'
 
 const variants = {
   hidden: { opacity: 0 },
@@ -45,29 +46,32 @@ const TodoList: React.FC<TodoListProps> = ({ todos, selectedTags }) => {
       : true
   )
   return (
-    <div className="flex flex-col px-1 py-1 min-h-[500px] max-h-[800px] w-[1200px] overflow-auto">
-      {filteredTodos.map((todo) => (
-        <motion.div
-          key={todo.id}
-          initial="hidden"
-          animate="visible"
-          exit="hidden"
-          variants={variants}
-        >
-          <Todo key={todo.id} todo={todo} />
-        </motion.div>
-      ))}
+    <>
+      <div className="flex flex-col px-1 py-1 min-h-[500px] max-h-[800px] w-[1200px] overflow-auto">
+        {filteredTodos.map((todo) => (
+          <motion.div
+            key={todo.id}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={variants}
+          >
+            <Todo key={todo.id} todo={todo} />
+          </motion.div>
+        ))}
+      </div>
+      <div>
+        <Button onClick={handleOpenDialogClick}>Add New Todo</Button>
 
-      <button onClick={handleOpenDialogClick}>Add New Todo</button>
-
-      {isDialogOpen && (
-        <NewTodoDialog
-          open={isDialogOpen}
-          onOpenChange={setIsDialogOpen}
-          modal={true}
-        />
-      )}
-    </div>
+        {isDialogOpen && (
+          <NewTodoDialog
+            open={isDialogOpen}
+            onOpenChange={setIsDialogOpen}
+            modal={true}
+          />
+        )}
+      </div>
+    </>
   )
 }
 
